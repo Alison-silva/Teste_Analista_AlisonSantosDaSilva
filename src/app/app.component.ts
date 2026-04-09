4d2a26cf66baae1b536e576dbd4d577f50822c84
Alison-silva <alison.ssilva@hotmail.com>
2026-04-08 23:58:46 -0300

    filtro em tempo real por nome e status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,30 @@ import { VendaService } from './service/venda.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
   jsonVendas: Venda[] = [];
+  vendasFiltro: Venda[] = [];
+
+  filtroNome: string = '';
+  filtroStatus: string = '';
 
   constructor(private vendaService: VendaService){}
 
   ngOnInit() {
     this.vendaService.listarVendas().subscribe(dados =>{
       this.jsonVendas = dados;
+      this.vendasFiltro = dados;
     });
+  }
+
+  filtro(){
+    this.vendasFiltro = this.vendaService.filtroVendas(
+      this.jsonVendas,
+      this.filtroNome,
+      this.filtroStatus
+    );
   }
 
 
--- src/app/service/venda.service.ts
+++ src/app/service/venda.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Venda } from '../model/venda';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,15 @@ export class VendaService {
 
   listarVendas(): Observable<any>{
     return this.http.get(this.jsonUrl);
+  }
+
+  filtroVendas(lista: Venda[], nome: string, status: string): Venda[]{
+    return lista.filter(venda => {
+      const buscarNome = venda.cliente.toLowerCase().includes(nome.toLowerCase());
+      const buscarStatus = status === '' || venda.status === status;
+      return buscarNome && buscarStatus;
+    });
+
   }
 
 }
